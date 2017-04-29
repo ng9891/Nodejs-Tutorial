391b92d3e165b72800323bccbecc32f2c21bf28a
vulty <davidng9891@gmail.com>
2017-04-29 15:09:12 -0500

    More comments

--- HelloWorld/app.js
+++ HelloWorld/app.js
@@ -17,20 +17,21 @@ console.log("Server is running");
 //Extended Hello world
 http.createServer(function(request,response){
 	var name = require('url').parse(request.url, true).query.name;
+	//Parse the request URL where name = some var and put it into 'name' variable. 
 	
 	if (name === undefined) 
 		name = 'world';
 	
 	if (name == 'burningbird'){
 		var file = 'test.png';
-		fs.stat(file,function(err, stat){
+		fs.stat(file,function(err, stat){ //fs.stat(path, callback)
 			if(err){ //if img not found
-				console.error(err);
+				console.error(err); //sends error into the console
 				response.writeHead(200,{'Content-Type':'text/plain'});
 				response.end("Sorry, no hot grill for you");
 			}
 			else{  //get img and post
-				var img = fs.readFileSync(file);
+				var img = fs.readFileSync(file); //read img Sync
 				response.contentType = 'image/png';
 				response.contentLenght = stat.size;
 				response.end(img, 'binary');
